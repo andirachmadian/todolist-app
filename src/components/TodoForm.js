cb00fdb29016a67298d697c8027f4ea084c188a0
andirachmadian <andikharisma24@gmail.com>
2022-06-10 15:14:45 +0700

    update todoForm

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
+    
 const [input, setInput] = useState('')
 
 const handleChange = e => {
@@ -14,6 +15,8 @@ const handleSubmit = e => {
         id: Math.floor(Math.random() * 10000),
         text: input
     })
+
+    setInput('')
 }
 
   return (
