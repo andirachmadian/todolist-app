8e5b17f585cf022b02792d0aa4d0c8e99792a59a
andirachmadian <andikharisma24@gmail.com>
2022-06-10 20:34:53 +0700

    update todo and todolist

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,16 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
-import { RiCloseCirlcleLine} from 'react-icons'
+import { RiCloseCircleLine } from 'react-icons/ri'
 import { TiEdit} from 'react-icons/ti'
 
-function Todo({ todos, completeTodo}) {
+function Todo({ todos, completeTodo }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     })
 
   return todos.map((todo, index) => (
-    <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={indext}>
+    <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
         <div key={todo.id} onClick={() => completeTodo(todo.id)}>
             {todo.text}
         </div>
